a3e45353b5ed60db661ba3b9c2584407b869e195
Reem Saijary <reemsaijary31@gmail.com>
2026-03-25 23:48:43 +0200

    Create products table

---
+++ database/migrations/2026_03_25_214122_create_products_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table -> string('product_name');
+            $table -> string('product_type')->nullable();
+            $table -> text('description') ->nullable();
+            $table -> decimal('unit_price',10,2);
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
